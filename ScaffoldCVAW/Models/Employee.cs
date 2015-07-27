1867b6fee1d2caa79d07f8f6f2dc9ea37eecd7b2
Syed Farjad Zia Zaidi <farjad_bullet@rocketmail.com>
2015-07-27 11:54:59 +0500

    Added AngularCRUD Template, Updated EntityFramework

--- ScaffoldCVAW/Models/Employee.cs
+++ ScaffoldCVAW/Models/Employee.cs
@@ -10,5 +10,16 @@ namespace AngularJSControllerScaffolding.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+        public virtual ICollection<EmployeeSalary> EmployeeSalaries { get; set; }
+    }
+
+    public class EmployeeSalary
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+
+        public int Amount { get; set; }
+
+        public virtual Employee Employee { get; set; }
     }
 }
